49c687fa33f7138665964f38bc1b9dbfaf6e4dd8
Jayyee-HPC <jie.yang@marquette.edu>
2021-12-12 13:44:00 -0800

    1212 back

--- src/adlbTest.cpp
+++ src/adlbTest.cpp
@@ -12,6 +12,14 @@
 #include <geos/geom/prep/PreparedGeometryFactory.h>
 #include <geos/geom/prep/PreparedGeometry.h>
 #include <geos/index/strtree/STRtree.h>
+#include <geos/geom/GeometryFactory.h>
+#include <geos/geom/Point.h>
+#include <geos/geom/Coordinate.h>
+#include <geos/geom/CoordinateArraySequence.h>
+#include <geos/geom/CoordinateArraySequenceFactory.h>
+#include <geos/geom/LinearRing.h>
+#include <geos/geom/LineString.h>
+#include <geos/geom/Polygon.h>
 
 #include "mpitype.h"
 #include "MpiReadStruct.h"
@@ -19,11 +27,149 @@
 #include "./spdlog/spdlog.h"
 #include "./spdlog/cfg/env.h" // support for loading levels from the environment variable#include "MpiReadStruct.h"
 
+#define DEBUG
 #define WORK 1000
 //mpicc -o prog nq.c libadlb.a libmpigis.a -lm
 //mpirun -np 2 ./prog 
 using namespace std;
 using namespace geos::geom;
+int num_types = 1;
+int types[2] = {WORK};
+
+#define TYPE_ID_UNKNOWN 0
+#define TYPE_ID_POINT 1
+#define TYPE_ID_LINESTRING 2
+#define TYPE_ID_POLYGON 3
+
+int get_geom_type_id(Geometry *geom)
+{
+	if (geom->getGeometryTypeId() == GEOS_POINT)
+		return TYPE_ID_POINT;
+	else if (geom->getGeometryTypeId() == GEOS_LINESTRING)
+		return TYPE_ID_LINESTRING;
+	else if (geom->getGeometryTypeId() == GEOS_POLYGON)
+		return TYPE_ID_POLYGON;
+	else
+		return TYPE_ID_UNKNOWN;
+}
+
+int join_adlb_task(double* data, int data_len)
+{
+	if (data == nullptr || data_len <= 0)return 0;
+	int num_geoms = (int)data[0];
+	vector<Geometry*> geoms;
+	geos::geom::CoordinateArraySequenceFactory csf;
+	int curr_pos = 1 + num_geoms * 2;
+	geos::geom::GeometryFactory::Ptr gf = geos::geom::GeometryFactory::create();
+
+	for (int i = 0; i < num_geoms; ++i)
+	{
+		int type = (int)data[1 + i * 2];
+		int size = (int)data[1 + i * 2 + 1];
+
+		geos::geom::Geometry *geo = NULL;
+        geos::geom::CoordinateArraySequence *coords_arr = new geos::geom::CoordinateArraySequence(size);
+
+		//spdlog::debug("{} {} {}", curr_pos, size, data_len);
+
+		for (int j = 0; j < size; ++j)
+		{
+			coords_arr->setAt(geos::geom::Coordinate(data[curr_pos + 2 * j], data[curr_pos + 2 * j + 1]), j);
+		}
+		curr_pos += (2 * size);
+
+		if (type == TYPE_ID_POLYGON)
+        {
+            //POLYGON
+            try
+            {
+                // Create non-empty LinearRing instance
+                geos::geom::LinearRing ring(coords_arr, gf->getDefaultInstance());
+                // Exterior (clone is required here because Polygon takes ownership)
+                geo = ring.clone().release();
+
+                geos::geom::LinearRing *exterior = dynamic_cast<geos::geom::LinearRing *>(geo);
+                std::unique_ptr<geos::geom::Polygon> poly(gf->getDefaultInstance()->createPolygon(exterior, nullptr));
+
+                //geos::geom::LinearRing* ring = new geos::geom::LinearRing(coords_arr, gf);
+                //std::unique_ptr<geos::geom::Polygon> poly(gf->createPolygon(ring, nullptr));
+                geo = dynamic_cast<geos::geom::Geometry *>(poly.release());
+
+                //delete coords_arr;
+            }
+            catch (std::exception &e)
+            {
+                spdlog::error("{}", e.what());
+            }
+        }
+        else if (type = TYPE_ID_LINESTRING)
+        {
+            //LINESTRING
+            try
+            {
+                std::unique_ptr<geos::geom::LineString> line(gf->getDefaultInstance()->createLineString(coords_arr));
+
+                geo = dynamic_cast<geos::geom::Geometry *>(line.release());
+            }
+            catch (std::exception &e)
+            {
+                spdlog::error("{}", e.what());
+            }
+        }
+        else if (type == TYPE_ID_POINT)
+        {
+            //POINT
+            try
+            {
+                std::unique_ptr<geos::geom::Point> point(gf->getDefaultInstance()->createPoint(coords_arr));
+                geo = dynamic_cast<geos::geom::Geometry *>(point.release());
+            }
+            catch (std::exception &e)
+            {
+                spdlog::error("{}", e.what());
+            }
+        }
+        else
+        {
+            //Unkown type, try to convert to polygon
+            spdlog::error("Parsing Unknown type: {}", type);
+
+            try
+            {
+                // Create non-empty LinearRing instance
+                geos::geom::LinearRing ring(coords_arr, gf->getDefaultInstance());
+                // Exterior (clone is required here because Polygon takes ownership)
+                geo = ring.clone().release();
+
+                geos::geom::LinearRing *exterior = dynamic_cast<geos::geom::LinearRing *>(geo);
+                std::unique_ptr<geos::geom::Polygon> poly(gf->getDefaultInstance()->createPolygon(exterior, nullptr));
+
+                //geos::geom::LinearRing* ring = new geos::geom::LinearRing(coords_arr, gf);
+                //std::unique_ptr<geos::geom::Polygon> poly(gf->createPolygon(ring, nullptr));
+                geo = dynamic_cast<geos::geom::Geometry *>(poly.release());
+            }
+            catch (std::exception &e)
+            {
+                spdlog::error("{}", e.what());
+            }
+        }
+
+		if (geo != nullptr)
+			geoms.push_back(geo);
+	}
+
+	std::unique_ptr<geos::geom::prep::PreparedGeometry> pg =
+                geos::geom::prep::PreparedGeometryFactory::prepare(geoms[0]);
+
+	int join_result = 0;
+
+	for (int i = 1; i < geoms.size(); ++i)
+		if (pg->intersects(geoms[i]))
+			++join_result;
+
+	return join_result;
+}
+
 void server_func(int my_world_rank)
 {
 	double time_t1, time_t2;
@@ -36,6 +182,12 @@ void server_func(int my_world_rank)
 
 void work_func(int my_world_rank, string file_path_1, string file_path_2, MPI_Comm worker_comm)
 {
+	int req_types[3], work_type, work_len;
+	int work_prio, work_handle[ADLB_HANDLE_SIZE], answer_rank;
+
+	req_types[0] = WORK;
+    req_types[1] = -1;
+
 	if(worker_comm != MPI_COMM_NULL) 
 	{
 		double tv_begin = MPI_Wtime();
@@ -67,24 +219,32 @@ void work_func(int my_world_rank, string file_path_1, string file_path_2, MPI_Co
 
 			if (!query_results.empty())
 			{
+				//string debug_str;
+
+				//debug_str += to_string(curr_geom->getNumPoints());
+				//debug_str.push_back(' ');
 				int num_geoms = 1 + query_results.size();
 				int num_points = curr_geom->getNumPoints();
 
+				//spdlog::debug("Size {}", query_results.size());
 				for (int i = 0; i < query_results.size(); ++i)
 				{
 					num_points += ((Geometry*)(query_results[i]))->getNumPoints();
+					//debug_str += to_string(((Geometry*)(query_results[i]))->getNumPoints());
+					//debug_str.push_back(' ');
 				}
 
 				// one extra double to record how many geometries in this buffer
 				double *temp_buf;
-				temp_buf = (double*)malloc((1 + num_geoms + 2 * num_points) * sizeof(double));
+				temp_buf = (double*)malloc((1 + 2 * num_geoms + 2 * num_points) * sizeof(double));
 
-				temp_buf[0]	= (double)(1 + num_geoms);
+				temp_buf[0]	= (double)(num_geoms);
 
-				int curr_pos = 1 + num_geoms;
+				int curr_pos = 1 + 2 * num_geoms;
 
 				// write the first geometry from dataset 1
-				temp_buf[1] = (double)(curr_geom->getNumPoints());
+				temp_buf[1] = (double)(get_geom_type_id(curr_geom));
+				temp_buf[2] = (double)(curr_geom->getNumPoints());
 				
 				std::unique_ptr< CoordinateSequence > temp_coords = curr_geom->getCoordinates();
 
@@ -99,7 +259,8 @@ void work_func(int my_world_rank, string file_path_1, string file_path_2, MPI_Co
 				for (int i = 0; i < query_results.size(); ++i)
 				{
 					std::unique_ptr< CoordinateSequence > temp_coords = ((Geometry*)query_results[i])->getCoordinates();
-					temp_buf[i+2] = temp_coords.get()->size();
+					temp_buf[3 + 2 * i] = get_geom_type_id((Geometry*)query_results[i]);
+					temp_buf[3 + 2 * i + 1] = temp_coords.get()->size();
 
 					for (int j = 0; j < temp_coords.get()->size(); ++j)
 					{
@@ -109,9 +270,53 @@ void work_func(int my_world_rank, string file_path_1, string file_path_2, MPI_Co
 					curr_pos += (2 * temp_coords.get()->size()); 
 				}
 
-				ADLB_Put(temp_buf, (num_geoms + 2 * num_points) * sizeof(double), -1, my_world_rank, WORK, 1);
+				//spdlog::debug("PUT {} : {}", (1 + 2 * num_geoms + 2 * num_points), debug_str);
+				ADLB_Put(temp_buf, (1 + 2 * num_geoms + 2 * num_points) * sizeof(double), -1, my_world_rank, WORK, 1);
 			}	
 		}
+		
+		// Workers begin to ask for tasks
+		int join_result = 0;
+		while (1)
+		{
+			int rc = ADLB_Reserve(req_types,&work_type,&work_prio,work_handle,
+                          &work_len,&answer_rank);;
+
+			// get no more work
+			if (rc == ADLB_NO_MORE_WORK)
+			{
+				spdlog::info("Rank {}, end with no more work", my_world_rank);
+				break;
+			}
+			else if (rc == ADLB_DONE_BY_EXHAUSTION)
+			{
+				spdlog::info("Rank {}, done by exhaustion", my_world_rank);
+				break;
+			}
+			else if (rc < 0)
+			{
+				spdlog::error("Rank {}, reserve failed", my_world_rank);
+				break;
+			}
+
+			if (work_type == WORK)
+			{
+				double * temp_buf;
+				temp_buf = (double *)malloc(work_len);
+				work_len /= sizeof(double);
+				rc = ADLB_Get_reserved(temp_buf, work_handle );
+
+				join_result += join_adlb_task(temp_buf, work_len);
+				//spdlog::debug("Rank {}, get work of length {}. {} {} {}", my_world_rank, work_len, temp_buf[0], temp_buf[1], temp_buf[2]);
+			}
+			else /* work type mismatch*/
+			{
+				spdlog::error("Rank {}, unknown work type", my_world_rank);
+				break;
+			}
+		}
+
+		spdlog::info("Rank {} finished, join result {}", my_world_rank, join_result);
 	}
 	else
 	{
@@ -128,8 +333,8 @@ int main(int argc, char *argv[])
 	MPI_Init(&argc,&argv);
 	MPI_Comm_size(MPI_COMM_WORLD,&num_world_nodes);
 
-	const string filepath1 = argv[2];
-	const string filepath2 = argv[3];
+	const string file_path_1 = argv[2];
+	const string file_path_2 = argv[3];
 
 
 	double start_tv;
@@ -154,10 +359,8 @@ int main(int argc, char *argv[])
 
 	MpiReadStruct mpiReader;
       
-	int num_types = 1;
-	int types[2] = {WORK};
 	const int use_debug_server = 0;
-	const int num_servers = 3;
+	const int num_servers = std::stoi(argv[1]);
 	int am_server;
 	int am_debug_server;
 	MPI_Comm app_comm;
@@ -211,137 +414,13 @@ int main(int argc, char *argv[])
 			num_workers--;
 
 		work_func(my_world_rank, file_path_1, file_path_2, worker_comm);
-	
+		ADLB_Finalize();
+	}
 
-	double endbuildindex_tv;
-	endbuildindex_tv = mpi_wtime();
-
-	int req_types[2];
-	req_types[0] = work;
-	req_types[1] = -1;
     
-	int work_type;
-	int work_prio;
-	int work_handle[adlb_handle_size];
-	int work_len;
-	int answer_rank;
-	long workcount = 0;
+	MPI_Barrier(worker_comm);
 	
-	while(1){
-		geos::io::wktreader wktreader;
-		list<geometry*> *g1 = new list<geometry*>;
-		long startflg = 0;
-		long endflg = 0;	
-		rc = adlb_reserve(req_types, &work_type, &work_prio, work_handle,
-                          &work_len, &answer_rank);
-
-    if(rc == adlb_no_more_work){
-      aprintf(1,"got no_more_work\n");
-      break;
-    }else if (rc == adlb_done_by_exhaustion){
-      aprintf(1,"got exhaustion\n");
-			break;
-    }else if (rc < 0){
-      aprintf(1,"**** reserve failed rc %d\n",rc);
-      exit(-1);
-    }else{
-			
-		}
-    char *work = (char *)malloc(sizeof(char) * work_len);
-		rc = adlb_get_reserved(work, work_handle);
-
-		for(int i = 0; i < work_len; i++){
-			if(work[i] != '\n'){
-				endflg++;
-			}else{
-				string* tmpstr = new string;
-				tmpstr->assign(&work[startflg], &work[endflg]);
-				
-				startflg = endflg + 1;
-				endflg = endflg + 1;
-				
-				try{
-					geometry* tmpgeo = null;
-					tmpgeo = wktreader.read(*tmpstr).release();
-				
-					delete tmpstr;
-					tmpstr = null;
-					if(tmpgeo->isvalid())
-						g1->push_back(tmpgeo);
-				}catch(exception &e){
-					
-				}
-			}			
-		}
-
-		free(work); 
-
-
-
-		for(list<geometry*>::iterator wkritr = g1->begin(); wkritr != g1->end(); wkritr ++){
-			geometry* tmpgeom = *wkritr;
-						
-			std::vector<void *> results;
-				
-			prep::preparedgeometryfactory pgf;
-				
-			const prep::preparedgeometry* ppdgeom = pgf.create(tmpgeom).release();
-				
-			index.query((*wkritr)->getenvelopeinternal(), results);
-
-			for(vector<void *>::iterator vditr = results.begin(); vditr != results.end(); vditr ++){
-				void *poly2ptr = *vditr;
-				geometry* qrdgeom = (geometry*)poly2ptr;
-				try{
-					if(ppdgeom->intersects(qrdgeom))						
-						workcount++;
-				}catch(exception &e){
-
-				}
-			}
-
-			//tmpgeom->~geometry();
-			pgf.destroy(ppdgeom);
-
-		}
-
-    delete g1;
-		g1 = null;
-    if (rc == adlb_no_more_work){
-      aprintf(1,"got no_more_work\n");
-      break;
-    }else if (rc == adlb_done_by_exhaustion){
-      aprintf(1,"got exhaustion\n");
-      break;
-    }else if (rc < 0){
-      aprintf(1,"**** reserve failed rc %d\n",rc);
-      exit(-1);
-     } 
-  } 
-        
-	adlb_finalize();
-    
-	double end_tv;
-	end_tv = mpi_wtime();
-	
-	
-	double timeforreadfile = endread_tv - start_tv;
-	double timeforbuildindex = endbuildindex_tv - endread_tv;
-	double timeforgeosoperation = end_tv - endbuildindex_tv;
-	
-	printf("%d, %f, %f, %f \n", my_world_rank, timeforreadfile, timeforbuildindex, timeforgeosoperation);
-	fflush(stdout);			
-	mpi_barrier(worker_comm);
-	
-	int total = 0;
-	
-	double maxtimeforreadfile = 0;
-	double maxtimeforbuldindex = 0;
-	double maxtimeforgeosoperation = 0;
-	double mintimeforreadfile = 0;
-	double mintimeforbuldindex = 0;
-	double mintimeforgeosoperation = 0;
-	
+	/*
 	mpi_reduce(&workcount, &total, 1, mpi_int, mpi_sum, 0, worker_comm);
 	mpi_reduce(&timeforreadfile, &maxtimeforreadfile, 1, mpi_double, mpi_max, 0, worker_comm);
 	mpi_reduce(&timeforbuildindex, &maxtimeforbuldindex, 1, mpi_double, mpi_max, 0, worker_comm);
@@ -357,11 +436,9 @@ int main(int argc, char *argv[])
 		printf("time for geometry operation = %f :: %f\n", maxtimeforgeosoperation, mintimeforgeosoperation);
 		fflush(stdout);
 	}
-		
-	}
-
-
-	mpi_finalize();
+	*/
+	spdlog::debug("Rank {} finished", my_world_rank);
+	MPI_Finalize();
 	return 0;
 }
 
